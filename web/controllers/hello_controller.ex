34676505419491b81577d8f96706f6d383de8a50
Jingjing Duan <jduan@twitter.com>
2015-12-27 18:45:16 -0800

    make the view customizable

--- web/controllers/hello_controller.ex
+++ web/controllers/hello_controller.ex
@@ -1,7 +1,7 @@
 defmodule HelloPhoenix.HelloController do
   use HelloPhoenix.Web, :controller
 
-  def world(conn, _params) do
-    render conn, "world.html"
+  def world(conn, %{"name" => name}) do
+    render conn, "world.html", name: name
   end
 end
